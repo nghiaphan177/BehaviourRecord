887ac51638c500781b06c686e21fcd3fd950ccb5
DuyLH4 <duylh4@fsoft.com.vn>
2022-07-14 04:32:46 +0700

    Xóa source để update source mới

--- BehaviourManagementSystem/BehaviourManagementSystem_ViewModels/Responses/Common/ResponceResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BehaviourManagementSystem_ViewModels.Responses.Common
-{
-    public class ResponceResult<Type>
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public Type Result { get; set; }
-    }
-}
