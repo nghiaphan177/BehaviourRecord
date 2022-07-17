63ef7b94a0274f2d0ce047eba0a0de6f1dc5cb36
HuyNQ6 <HuyNQ6@fsoft.com.vn>
2022-07-17 17:41:43 +0700

    Thêm Js, sửa layout teacher đánh giá

---
+++ BehaviourManagementSystem_MVC/wwwroot/js/ChuyenTabDanhGia.js
@@ -0,0 +1,24 @@
+﻿$('#button-TuongTrinh').click(function (e) {
+    e.preventDefault();
+    $('.nav a[href="#phantich"]').tab('show');
+});
+
+$('#button-pt1').click(function (e) {
+    e.preventDefault();
+    $('.nav a[href="#pt-tiende"]').tab('show');
+});
+
+$('#button-pt2').click(function (e) {
+    e.preventDefault();
+    $('.nav a[href="#pt-hauqua"]').tab('show');
+});
+
+$('#button-pt3').click(function (e) {
+    e.preventDefault();
+    $('.nav a[href="#chitiet"]').tab('show');
+});
+
+$('#button-ct1').click(function (e) {
+    e.preventDefault();
+    $('.nav a[href="#ct-hauqua"]').tab('show');
+});
---
+++ BehaviourManagementSystem_MVC/wwwroot/js/DropDown.js
@@ -0,0 +1,20 @@
+﻿$('#multiselect-drop-camxuc input').change(function () {
+    var s = $('#multiselect-drop-camxuc input:checked').map(function () {
+        return this.value;
+    }).get().join(',');
+    $('#camxuc').val((s.length > 0 ? s : ""));
+});
+
+$('#multiselect-drop-moitruong input').change(function () {
+    var s = $('#multiselect-drop-moitruong input:checked').map(function () {
+        return this.value;
+    }).get().join(',');
+    $('#moitruong').val((s.length > 0 ? s : ""));
+});
+
+$('#multiselect-drop-hanhdong input').change(function () {
+    var s = $('#multiselect-drop-hanhdong input:checked').map(function () {
+        return this.value;
+    }).get().join(',');
+    $('#hanhdong').val((s.length > 0 ? s : ""));
+});
