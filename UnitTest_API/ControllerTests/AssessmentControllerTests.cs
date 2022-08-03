3329c529c9524c2a2d6f50b62ce89824044dd92d
HoangDDN <HoangDDN@CVPHOANGDDN-27>
2022-08-03 16:59:48 +0700

    Update Intervention Unit test and comment

--- UnitTest_API/ControllerTests/AssessmentControllerTests.cs
+++ UnitTest_API/ControllerTests/AssessmentControllerTests.cs
@@ -15,6 +15,12 @@ using Xunit;
 
 namespace UnitTest_API.ControllerTests
 {
+    /// <summary>
+    /// Assessment_UnitTest
+    /// writter: HoangDDN
+    /// Description: GetAll,AddRecord,UpdateRecord,UpdateAnalyzeAntecedent,UpdateAnalyzeConsequence,UpdateAnalyzeBehaviour,
+    /// Description: UpdateFuntionAntecedent,UpdateFuntionConsequence,Remove,Detail
+    /// </summary>
     public class AssessmentControllerTests
     {
         private readonly AssessmentController _controller;
@@ -24,9 +30,9 @@ namespace UnitTest_API.ControllerTests
             _service = new AssessmentServiceFakes();
             _controller = new AssessmentController(_service);
         }
-
+        // Test GetAll Assessment với Id tồn tại và trả về Ok
         [Fact]
-        public async void GetAll_Assessment_ThenOk_ExistingId_Test()
+        public async void GetAll_Assessment_ExistingId_ThenOk_Test()
         {
             // Arrange
             var ind_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c100";
@@ -38,7 +44,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal(200, okResult.StatusCode);
             Assert.IsType<List<AssessmentRequest>>(item.Result);
         }
-
+        // Test GetAll Assessment với Id không tồn tại và trả về Badrequest
         [Fact]
         public async Task GetAll_Assessment_WithNotExistingId_ThenBadRequest_Test()
         {
@@ -52,7 +58,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal("Hiện tại không có dữ liệu", item.Message);
             Assert.IsType<BadRequestObjectResult>(badResponse);
         }
-
+        // Test Detail Assessment với Id tồn tại và trả về Ok
         [Fact]
         public async void Detail_Assessment_ExistingId_ThenOk_Test()
         {
@@ -65,7 +71,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal(200, okResult.StatusCode);
             Assert.IsType<AssessmentRequest>(item.Result);
         }
-
+        // Test Detail Assessment với Id không tồn tại và trả về Badrequest
         [Fact]
         public async Task Detail_Assessment_WithNotExistingId_ThenBadRequest_Test()
         {
@@ -79,7 +85,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal("Id assessment không tồn tại", item.Message);
             Assert.IsType<BadRequestObjectResult>(badResponse);
         }
-
+        // Test Add Assessment Record và trả về Ok
         [Fact]
         public async void Add_AssessmentRecord_ThenOk_Test()
         {
@@ -97,7 +103,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal(200, createdResponse.StatusCode);
             Assert.IsType<Assessment>(item.Result);
         }
-
+        // Test Add Assessment Record với Id không tồn tại và trả về Badrequest
         [Fact]
         public async void Add_AssessmentRecord_WithNotExistingIndId_ThenBadRequest_Test()
         {
@@ -116,7 +122,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal("Id individual không tồn tại", item.Message);
             Assert.IsType<BadRequestObjectResult>(badResponse);
         }
-
+        // Test Add Assessment Record với dữ liệu Null và trả về Badrequest
         [Fact]
         public async void Add_AssessmentRecord_WithNullData_ThenBadRequest_Test()
         {
@@ -135,7 +141,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal("Chưa có dữ liệu", item.Message);
             Assert.IsType<BadRequestObjectResult>(badResponse);
         }
-
+        // Test Update Assessment Record và trả về Ok
         [Fact]
         public async void Update_AssessmentRecord_ThenOk_Test()
         {
@@ -153,7 +159,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal(200, okResult.StatusCode);
             Assert.IsType<Assessment>(item.Result);
         }
-
+        // Test Update Assessment Record với Id không tồn tại và trả về Badrequest
         [Fact]
         public async void Update_AssessmentRecord_WithNotExistingAssId_ThenBadRequest_Test()
         {
@@ -172,7 +178,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal("Id assessment không tồn tại", item.Message);
             Assert.IsType<BadRequestObjectResult>(badResponse);
         }
-
+        // Test Update Assessment Record với dữ liệu Null và trả về Badrequest
         [Fact]
         public async void Update_AssessmentRecord_WithNullData_ThenBadRequest_Test()
         {
@@ -191,7 +197,239 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal("Chưa có dữ liệu", item.Message);
             Assert.IsType<BadRequestObjectResult>(badResponse);
         }
-
+        // Test Update Assessment Analyze Antecedent và trả về Ok
+        [Fact]
+        public async void Update_AssessmentAnalyzeAntecedent_ThenOk_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string ana_ant_per = "Tức giận";
+            string ana_ant_envi = "Nhiều người xung quanh";
+            string ana_ant_act = "Chạy trốn";
+            // Act
+            var okResult = await _controller.UpdateAnalyzeAntecedent(ass_id, ana_ant_per, ana_ant_envi, ana_ant_act) as OkObjectResult;
+            var item = (ResponseResultSuccess<Assessment>)okResult.Value;
+            // Assert
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.IsType<Assessment>(item.Result);
+        }
+        // Test Update Assessment Analyze Antecedent với Id không tồn tại và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentAnalyzeAntecedent_WithNotExistingAssId_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id_NotExisting = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c201";
+            string ana_ant_per = "Tức giận";
+            string ana_ant_envi = "Nhiều người xung quanh";
+            string ana_ant_act = "Chạy trốn";
+            // Act
+            var badResponse = await _controller.UpdateAnalyzeAntecedent(ass_id_NotExisting, ana_ant_per, ana_ant_envi, ana_ant_act) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Id assessment không tồn tại", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Analyze Antecedent với dữ liệu Null và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentAnalyzeAntecedent_WithNullData_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string ana_ant_per = null;
+            string ana_ant_envi = null;
+            string ana_ant_act = null;
+            // Act
+            var badResponse = await _controller.UpdateAnalyzeAntecedent(ass_id, ana_ant_per, ana_ant_envi, ana_ant_act) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Chưa có dữ liệu", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Analyze Consequence và trả về Ok
+        [Fact]
+        public async void Update_AssessmentAnalyzeConsequence_ThenOk_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string ana_con_per = "Tức giận";
+            string ana_con_envi = "Nhiều người xung quanh";
+            string ana_con_act = "Chạy trốn";
+            // Act
+            var okResult = await _controller.UpdateAnalyzeAntecedent(ass_id, ana_con_per, ana_con_envi, ana_con_act) as OkObjectResult;
+            var item = (ResponseResultSuccess<Assessment>)okResult.Value;
+            // Assert
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.IsType<Assessment>(item.Result);
+        }
+        // Test Update Assessment Analyze Consequence với Id không tồn tại và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentAnalyzeConsequence_WithNotExistingAssId_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id_NotExisting = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c201";
+            string ana_con_per = "Tức giận";
+            string ana_con_envi = "Nhiều người xung quanh";
+            string ana_con_act = "Chạy trốn";
+            // Act
+            var badResponse = await _controller.UpdateAnalyzeAntecedent(ass_id_NotExisting, ana_con_per, ana_con_envi, ana_con_act) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Id assessment không tồn tại", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Analyze Consequence với dữ liệu Null và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentAnalyzeConsequence_WithNullData_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string ana_con_per = null;
+            string ana_con_envi = null;
+            string ana_con_act = null;
+            // Act
+            var badResponse = await _controller.UpdateAnalyzeAntecedent(ass_id, ana_con_per, ana_con_envi, ana_con_act) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Chưa có dữ liệu", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Analyze Behaviour và trả về Ok
+        [Fact]
+        public async void Update_AssessmentAnalyzeBehaviour_ThenOk_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string ana_behaviour = "Đánh nhau";
+            // Act
+            var okResult = await _controller.UpdateAnalyzeBehaviour(ass_id, ana_behaviour) as OkObjectResult;
+            var item = (ResponseResultSuccess<Assessment>)okResult.Value;
+            // Assert
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.IsType<Assessment>(item.Result);
+        }
+        // Test Update Assessment Analyze Behaviour với Id không tồn tại và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentAnalyzeBehaviour_WithNotExistingAssId_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id_NotExisting = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c201";
+            string ana_behaviour = "Đánh nhau";
+            // Act
+            var badResponse = await _controller.UpdateAnalyzeBehaviour(ass_id_NotExisting, ana_behaviour) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Id assessment không tồn tại", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Analyze Behaviour với dữ liệu Null và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentAnalyzeBehaviour_WithNullData_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string ana_behaviour = null;
+            // Act
+            var badResponse = await _controller.UpdateAnalyzeBehaviour(ass_id, ana_behaviour) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Chưa có dữ liệu", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Funtion Antecendent và trả về Ok
+        [Fact]
+        public async void Update_AssessmentFuntionAntecedent_ThenOk_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string fun_ant = "Đánh bạn bị thương nhập viện";
+            // Act
+            var okResult = await _controller.UpdateFuntionAntecedent(ass_id, fun_ant) as OkObjectResult;
+            var item = (ResponseResultSuccess<Assessment>)okResult.Value;
+            // Assert
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.IsType<Assessment>(item.Result);
+        }
+        // Test Update Assessment Funtion Antecendent với Id không tồn tại và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentFuntionAntecedent_WithNotExistingAssId_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id_NotExisting = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c201";
+            string fun_ant = "Đánh bạn bị thương nhập viện";
+            // Act
+            var badResponse = await _controller.UpdateFuntionAntecedent(ass_id_NotExisting, fun_ant) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Id assessment không tồn tại", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Funtion Antecendent với dữ liệu Null và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentFuntionAntecedent_WithNullData_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string fun_ant = null;
+            // Act
+            var badResponse = await _controller.UpdateFuntionAntecedent(ass_id, fun_ant) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Chưa có dữ liệu", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Funtion Consequence và trả về Ok
+        [Fact]
+        public async void Update_AssessmentFuntionConsequence_ThenOk_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string fun_con = "Sau đó chạy trốn khỏi hiện trường";
+            // Act
+            var okResult = await _controller.UpdateFuntionConsequece(ass_id, fun_con) as OkObjectResult;
+            var item = (ResponseResultSuccess<Assessment>)okResult.Value;
+            // Assert
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.IsType<Assessment>(item.Result);
+        }
+        // Test Update Assessment Funtion Consequence với Id không tồn tại và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentFuntionConsequence_WithNotExistingAssId_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id_NotExisting = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c201";
+            string fun_con = "Sau đó chạy trốn khỏi hiện trường";
+            // Act
+            var badResponse = await _controller.UpdateFuntionAntecedent(ass_id_NotExisting, fun_con) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Id assessment không tồn tại", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Update Assessment Funtion Consequence với dữ liệu Null và trả về Badrequest
+        [Fact]
+        public async void Update_AssessmentFuntionConsequence_WithNullData_ThenBadRequest_Test()
+        {
+            // Arrange
+            string ass_id = "ab2bd817-98cd-4cf3-a80a-53ea0cd9c200";
+            string fun_con = null;
+            // Act
+            var badResponse = await _controller.UpdateFuntionAntecedent(ass_id, fun_con) as BadRequestObjectResult;
+            var item = (ResponseResultError<Assessment>)badResponse.Value;
+            // Assert
+            Assert.Equal(400, badResponse.StatusCode);
+            Assert.Equal("Chưa có dữ liệu", item.Message);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        // Test Remove Assessment và trả về Ok
         [Fact]
         public async void Remove_Assessment_ThenOk_Test()
         {
@@ -204,7 +442,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal(200, okResult.StatusCode);
             Assert.IsType<List<Assessment>>(item.Result);
         }
-
+        // Test Remove Assessment với Id không tồn tại và trả về Badrequest
         [Fact]
         public async void Remove_Assessment_WithNotExistingId_ThenBadRequest_Test()
         {
@@ -218,7 +456,7 @@ namespace UnitTest_API.ControllerTests
             Assert.Equal("Id không tồn tại",item.Message);
             Assert.IsType<BadRequestObjectResult>(badResponse);
         }
-
+        // Test Remove All Assessment và trả về Ok
         [Fact]
         public async void Remove_AllAssessment_ThenOk_Test()
         {
---
+++ UnitTest_API/Services/InterventionServiceFakes.cs
@@ -0,0 +1,202 @@
+﻿using BehaviourManagementSystem_API.Models;
+using BehaviourManagementSystem_API.Services;
+using BehaviourManagementSystem_ViewModels.Requests;
+using BehaviourManagementSystem_ViewModels.Responses.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest_API.Services
+{
+    class InterventionServiceFakes : IInterventionService
+    {
+        private readonly List<Intervention> _intervention;
+        private readonly List<Assessment> _assessment;
+
+
+        public InterventionServiceFakes()
+        {
+            _intervention = new List<Intervention>()
+            {
+                new Intervention()
+                {
+                   Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c300"),
+                   AssesetmentId = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
+                   ProfileMildDesciption = "Nhắc nhở, chép phạt",
+                   ProfileRecoveryDesciption = "Quản chế",
+                   ManageMild = "Nhắc nhở học sinh, chép phạt ko tái phạm",
+                   ManageRecovery = "Quản chế học sinh trong phạm vi trường học",
+                   PreventStatus = "Chống đối, nổi loạn",
+                   PreventActivity = "Hạn chế để học sinh tham gia các tiết học thể dục",
+                   PreventInteraction = "Công tác tư tưởng cho học sinh về tác hại của đánh nhau",
+                   PreventInvironmental = " ",
+                   CreateDate=DateTime.Now,
+                   UpdateDate=DateTime.Now
+                }
+            };
+            _assessment = new List<Assessment>()
+            {
+                new Assessment()
+                {
+                    Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
+                }
+            };
+        }
+
+        public async Task<ResponseResult<Intervention>> CreateProfile(string ass_id, DateTime p_date, string p_mild, string p_moder, string p_extre, string p_reco)
+        {
+            var a = new Intervention();
+            if (! _assessment.Any(prop => prop.Id.ToString() == ass_id))
+                return new ResponseResultError<Intervention>("Id assessment không tồn tại");
+            if (p_date.ToString() == null || p_reco == null)
+            {
+                return new ResponseResultError<Intervention>("Chưa có dữ liệu");
+            }
+            else
+            {
+                 _intervention.Add(a = new Intervention()
+                {
+                    Id = Guid.NewGuid(),
+                    ProfileDate = p_date,
+                    ProfileMildDesciption = p_mild,
+                    ProfileModerateDesciption = p_moder,
+                    ProfileExtremeDesciption = p_extre,
+                    ProfileRecoveryDesciption = p_reco,
+                    AssesetmentId = new Guid(ass_id),
+                    CreateDate = DateTime.Now,
+
+                    ProfileIsCompeleted = true
+                });
+            }
+
+            return new ResponseResultSuccess<Intervention>(a);
+        }
+
+        public async Task<ResponseResult<List<Intervention>>> Delete(string int_id)
+        {
+            if (! _intervention.Any(prop => prop.Id.ToString() == int_id))
+                return new ResponseResultError<List<Intervention>>("Id intervention không tồn tại");
+            var obj = _intervention.Find(prop => prop.Id.ToString() == int_id);
+            _intervention.Remove(obj);
+            return new ResponseResultSuccess<List<Intervention>>(_intervention.ToList());
+        }
+
+        public async Task<ResponseResult<InterventionRequest>> Detail(string int_id)
+        {
+            if (!_intervention.Any(prop => prop.Id.ToString() == int_id))
+                return new ResponseResultError<InterventionRequest>("Id intervention không tồn tại");
+            var obj = _intervention.Find(prop => prop.Id.ToString() == int_id);
+            return new ResponseResultSuccess<InterventionRequest>(new InterventionRequest()
+            {
+                Id = obj.Id.ToString(),
+                ProfileDate = obj.ProfileDate.GetValueOrDefault(),
+                ProfileMildDesciption = obj.ProfileMildDesciption,
+                ProfileModerateDesciption = obj.ProfileModerateDesciption,
+                ProfileExtremeDesciption = obj.ProfileExtremeDesciption,
+                ProfileRecoveryDesciption = obj.ProfileRecoveryDesciption,
+                ManageMild = obj.ManageMild,
+                ManageModerate = obj.ManageModerate,
+                ManageExtreme = obj.ManageExtreme,
+                ManageRecovery = obj.ManageRecovery,
+                PreventStatus = obj.PreventStatus,
+                PreventActivity = obj.PreventActivity,
+                PreventInteraction = obj.PreventInteraction,
+                PreventInvironmental = obj.PreventInvironmental,
+                AssesetmentId = obj.AssesetmentId.ToString()
+            });
+        }
+
+        public async Task<ResponseResult<List<InterventionRequest>>> GetAll(string ass_id)
+        {
+            var find = _intervention.Where(p => p.AssesetmentId.ToString() == ass_id);
+            var intervention = _intervention.Take(find.Count());
+            if (intervention.Any() == false)
+            {
+                return new ResponseResultError<List<InterventionRequest>>("Hiện tại không có dữ liệu");
+            }
+            var result = new List<InterventionRequest>();
+            foreach (var item in find)
+            {
+                result.Add(new InterventionRequest()
+                {
+                    Id = item.Id.ToString(),
+                    ProfileDate = item.ProfileDate.GetValueOrDefault(),
+                    ProfileIsCompeleted = item.ProfileIsCompeleted,
+                    ManageIsCompleted = item.ManageIsCompleted,
+                    PreventIsCompleted = item.PreventIsCompleted,
+                    Summary = item.Summary,
+                    AssesetmentId = item.AssesetmentId.ToString(),
+                });
+            }
+            return new ResponseResultSuccess<List<InterventionRequest>>(result);
+        }
+
+        public async Task<ResponseResult<Intervention>> UpdateManage(string int_id, string m_mild, string m_moder, string m_extre, string m_reco)
+        {
+            if (! _intervention.Any(prop => prop.Id.ToString() == int_id))
+                return new ResponseResultError<Intervention>("Id intervention không tồn tại");
+            var obj = _intervention.Find(prop => prop.Id.ToString() == int_id);
+            if (m_reco == null)
+            {
+                return new ResponseResultError<Intervention>("Chưa có dữ liệu");
+            }
+            else
+            {
+                obj.ManageMild = m_mild;
+                obj.ManageModerate = m_moder;
+                obj.ManageExtreme = m_extre;
+                obj.ManageRecovery = m_reco;
+                obj.ManageIsCompleted = true;
+
+                obj.UpdateDate = DateTime.Now;
+            }
+            return new ResponseResultSuccess<Intervention>(obj);
+        }
+
+        public async Task<ResponseResult<Intervention>> UpdatePrevent(string int_id, string pre_status, string pre_act, string pre_envi, string pre_inter)
+        {
+            if (! _intervention.Any(prop => prop.Id.ToString() == int_id))
+                return new ResponseResultError<Intervention>("Id intervention không tồn tại");
+            var obj = _intervention.Find(prop => prop.Id.ToString() == int_id);
+            if (pre_status == null || pre_act == null || pre_envi == null || pre_inter == null)
+            {
+                return new ResponseResultError<Intervention>("Chưa có dữ liệu");
+            }
+            else
+            {
+                obj.PreventStatus = pre_status;
+                obj.PreventActivity = pre_act;
+                obj.PreventInvironmental = pre_envi;
+                obj.PreventInteraction = pre_inter;
+
+                obj.PreventIsCompleted = true;
+                obj.UpdateDate = DateTime.Now;
+            }
+            return new ResponseResultSuccess<Intervention>(obj);
+        }
+
+        public async Task<ResponseResult<Intervention>> UpdateProfile(string int_id, DateTime p_date, string p_mild, string p_moder, string p_extre, string p_reco)
+        {
+            if (! _intervention.Any(prop => prop.Id.ToString() == int_id))
+                return new ResponseResultError<Intervention>("Id intervention không tồn tại");
+            var obj = _intervention.Find(prop => prop.Id.ToString() == int_id);
+            if (p_date.ToString() == null || p_reco == null)
+            {
+                return new ResponseResultError<Intervention>("Chưa có dữ liệu");
+            }
+            else
+            {
+                obj.ProfileDate = p_date;
+                obj.ProfileMildDesciption = p_mild;
+                obj.ProfileModerateDesciption = p_moder;
+                obj.ProfileExtremeDesciption = p_extre;
+                obj.ProfileRecoveryDesciption = p_reco;
+
+                obj.UpdateDate = DateTime.Now;
+            }
+            return new ResponseResultSuccess<Intervention>(obj);
+        }
+    }
+}
