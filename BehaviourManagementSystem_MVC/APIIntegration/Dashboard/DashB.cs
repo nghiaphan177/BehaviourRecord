26aad62c5aef706d3c2a915a3dc635a4d5831751
DucPM42 <DucPM42@fsoft.com.vn>
2022-08-05 14:25:39 +0700

    add file dasd

---
+++ BehaviourManagementSystem_MVC/APIIntegration/Dashboard/DashB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BehaviourManagementSystem_MVC.APIIntegration.Dashboard
+{
+    public class DashB
+    {
+    }
+}
---
+++ BehaviourManagementSystem_MVC/APIIntegration/Dashboard/IDashB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BehaviourManagementSystem_MVC.APIIntegration.Dashboard
+{
+    public interface IDashB
+    {
+        Task<ResponseResult<List<Tuple<int, int>>>> GetCountAllAccountRegisterOfYear(string year);
+
+        Task<ResponseResult<List<Tuple<int, int>>>> GetCountAllAccountRegisterOfMonth(int m, int y);
+
+        Task<ResponseResult<List<Tuple<string, int>>>> GetCountAllStudentOfAllClasses(Guid guid);
+
+        Task<ResponseResult<Tuple<int, int>>> GetAllAccountNotVerifyMail();
+
+        Task<ResponseResult<Tuple<int, int, int>>> GetAllStudentAndTeacherAndAllAccount();
+
+        Task<ResponseResult<List<Tuple<int, int, int>>>> GetAllAssessAndInterByMonthWithTeacher(Guid teacherid);
+    }
+}
